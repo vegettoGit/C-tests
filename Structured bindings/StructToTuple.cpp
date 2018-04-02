e732d3c55802ee0275c10936ac89248a6f97a260
Diego <diego.delpozo@gmail.com>
2018-04-02 10:24:39 +0100

    Enhancements and comments on our Structure to Tuple test

--- Structured bindings/StructToTuple.cpp
+++ Structured bindings/StructToTuple.cpp
@@ -4,10 +4,12 @@
 #include <tuple>
 #include <cassert>
 
+// Can we construct type T with braces by passing over the specified arguments? If so, we get true_type
 template <class T, class... TArgs> 
 decltype(T{ std::declval<TArgs>()... }, std::true_type{}) 
 test();
 
+// Calling isBracesConstructible with any other format will give us false_type
 template <class, class...> 
 std::false_type 
 test(...);
@@ -17,6 +19,7 @@ using isBracesConstructible = decltype(test<T, TArgs...>());
 
 struct anyType 
 {
+    // Conversion operator to interoperable C++ types
     template<class T>
     constexpr operator T();
 };
@@ -26,7 +29,7 @@ auto toTuple(T&& object) noexcept
 {
     using type = std::decay_t<T>;
     if constexpr(isBracesConstructible<type, anyType, anyType>{}) 
-	{
+    {
         auto&&[member1, member2] = object;
         return std::make_tuple(member1, member2);
     }
@@ -35,16 +38,15 @@ auto toTuple(T&& object) noexcept
 int main()
 {
     struct myStruct
-	{
+    {
         float  myFloat;
         double myDouble;
     };
 
     auto myTuple = toTuple(myStruct{ 1.0f, 2.0 });
-		
+        
     static_assert(std::is_same<std::tuple<float, double>, decltype(myTuple)>{}, "The structToTuple type is not the same as the tuple type");
     assert(std::get<0>(myTuple) == 1.0f && std::get<1>(myTuple) == 2.0);
 
     return 0;
 }
-
