5b2a5e7e1a0e123ee15119d17011c4e1166f745c
Diego <diego.delpozo@gmail.com>
2021-01-17 12:50:30 +0100

    MoveTests.h indentation fixes

--- Move semantics/MoveTests.h
+++ Move semantics/MoveTests.h
@@ -4,20 +4,20 @@ struct MoveTest1
 {
    MoveTest1              ();
    MoveTest1              (const char* name);
-	~MoveTest1             ();
+   ~MoveTest1             ();
 
-	MoveTest1              (const MoveTest1&);
-	MoveTest1              (MoveTest1&&);
+   MoveTest1              (const MoveTest1&);
+   MoveTest1              (MoveTest1&&);
 
-	MoveTest1&  operator = (const MoveTest1&);
-	MoveTest1&  operator = (MoveTest1&&);
+   MoveTest1&  operator = (const MoveTest1&);
+   MoveTest1&  operator = (MoveTest1&&);
 
-	const char* getName    () const;
+   const char* getName    () const;
 
-	void        printName  () const;
+   void        printName  () const;
 
 private:
 
-	std::string m_name;
-	char*       m_singleChar;
+   std::string m_name;
+   char*       m_singleChar;
 };
